91784aa6cf351ae6bd4c4c611084f8fb6dc0c30d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-12 22:21:10 -0300

    comits do professor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.4.1")
 }
---
+++ app/src/main/java/gabiterzi/github/com/lista_de_compras_2sem/ItemModel.kt
@@ -0,0 +1,6 @@
+package carreiras.com.github.kotlin_android_lista_de_compras
+Model(
+    val id: Int,
+    val name: String,
+    val onRemove: (ItemModel) -> Unit
+)
